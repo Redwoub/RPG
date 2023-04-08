e9db27f7ab044ebc157249c7d8a91a93165d3eb6
Redwoub <eloiverinaudcoma@gmail.com>
2023-04-08 18:15:44 +0100

    [add] fonction createFile and removeLineFromFile

--- src/fr/redwoub/mydoriarpg/utils/FileUtils.java
+++ src/fr/redwoub/mydoriarpg/utils/FileUtils.java
@@ -1,10 +1,11 @@
 package fr.redwoub.mydoriarpg.utils;
 
 import fr.redwoub.mydoriarpg.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 public class FileUtils {
 
@@ -34,5 +35,36 @@ public class FileUtils {
             // delete files and empty subfolders
             subfile.delete();
         }
+    }
+
+    public static void createFile(File file){
+        if(file.exists()) return;
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            Bukkit.getConsoleSender().sendMessage("§cFailed to create file named §7: §b" + file.getName() + "\n" + new RuntimeException(e));
+        }
+    }
+
+    public static void removeLine(String lineToRemove, File file) throws IOException{
+        //Reading File Content and storing it to a StringBuilder variable ( skips lineToRemove)
+        StringBuilder sb = new StringBuilder();
+        try (Scanner sc = new Scanner(file)) {
+            String currentLine;
+            while(sc.hasNext()){
+                currentLine = sc.nextLine();
+                if(currentLine.equalsIgnoreCase(lineToRemove)){
+                    continue; //skips lineToRemove
+                }
+                sb.append(currentLine).append("\n");
+            }
+        }
+        //Delete File Content
+        PrintWriter pw = new PrintWriter(file);
+        pw.close();
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
+        writer.append(sb.toString());
+        writer.close();
     }
 }
