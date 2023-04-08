0ee74696d99d1d499b04eb7542371e6c7889f688
Redwoub <eloiverinaudcoma@gmail.com>
2023-04-08 21:55:17 +0100

    Update LevelUP.java

--- src/fr/redwoub/mydoriarpg/commands/LevelUP.java
+++ src/fr/redwoub/mydoriarpg/commands/LevelUP.java
@@ -2,6 +2,8 @@ package fr.redwoub.mydoriarpg.commands;
 
 import fr.redwoub.mydoriarpg.Main;
 import fr.redwoub.mydoriarpg.managers.RpgLevelManager;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,11 +15,27 @@ public class LevelUP implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
         if(!(sender instanceof Player)) return false;
-        Player player = (Player) sender;
-        Main.getInstance().getAccount(player).ifPresent(accounts -> {
-            RpgLevelManager rpgLevelManager = new RpgLevelManager(accounts);
-            rpgLevelManager.levelUp();
-        });
+
+        if(args.length < 1){
+            Player player = (Player) sender;
+            Main.getInstance().getAccount(player).ifPresent(account -> {
+                RpgLevelManager rpgLevelManager = new RpgLevelManager(account);
+                rpgLevelManager.levelUp();
+            });
+        } else if(args.length == 1){
+            if(Bukkit.getPlayer(args[0]) != null){
+                Player player = Bukkit.getPlayer(args[0]);
+                Main.getInstance().getAccount(player).ifPresent(account -> {
+                    RpgLevelManager rpgLevelManager = new RpgLevelManager(account);
+                    rpgLevelManager.levelUp();
+                });
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Main.getInstance().getConfig().getString("messages.system-prefix")) + "§e" + player.getName() + " §aa bien levelup");
+            } else {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Main.getInstance().getConfig().getString("messages.system-prefix")) + "§cErreur §8: §cCe joueur n'existe pas");
+                return false;
+            }
+        }
+
         return false;
     }
 }
