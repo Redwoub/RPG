8b9f12ce3f5368702e8379a079400327d76c47a8
Redwoub <eloiverinaudcoma@gmail.com>
2023-04-09 15:04:35 +0100

    [update] fix bug + add nickname reset

--- src/fr/redwoub/mydoriarpg/commands/Nick.java
+++ src/fr/redwoub/mydoriarpg/commands/Nick.java
@@ -1,6 +1,7 @@
 package fr.redwoub.mydoriarpg.commands;
 
 import fr.redwoub.mydoriarpg.Main;
+import fr.redwoub.mydoriarpg.accounts.Accounts;
 import fr.redwoub.mydoriarpg.utils.FileUtils;
 import fr.redwoub.mydoriarpg.utils.PlayerUtils;
 import org.bukkit.ChatColor;
@@ -10,6 +11,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
+
 public class Nick implements CommandExecutor {
 
     @Override
@@ -18,20 +20,44 @@ public class Nick implements CommandExecutor {
         if(!(sender instanceof Player)) return false;
 
         Player player = (Player) sender;
-        if(args.length != 1){
+        if(Main.getInstance().getAccount(player).isPresent()){
+            Accounts accounts = Main.getInstance().getAccount(player).get();
+            if(accounts.getDataLvl().getRpgLvl() < 1000){
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', Main.getInstance().getConfig().getString("messages.system-prefix")) + "§cErreur §8: §cIl faut être rpgLevel 1000 pour pouvoir utilisé cette commande");
+                return false;
+            }
+        }
+        if(args.length == 0){
+            Main.getInstance().getAccount(player).ifPresent(account -> {
+                account.getDataStatistique().setNickname(player.getName());
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', Main.getInstance().getConfig().getString("messages.system-prefix")) + "§aVotre nickname a été réinitialisé");
+                player.setDisplayName(PlayerUtils.getByString(account.getDataStatistique().getColorName()) + account.getDataStatistique().getNickname());
+            });
+            YamlConfiguration config = YamlConfiguration.loadConfiguration(Main.getInstance().getNicknameWithUUID());
+            config.set(player.getUniqueId().toString(), player.getName());
+            FileUtils.save(config, Main.getInstance().getNicknameWithUUID());
+            if(!Main.getInstance().getUuidLinkedToNickname().isEmpty())
+                Main.getInstance().getUuidLinkedToNickname().clear();
+            FileUtils.fillMapWithAllLineOfFile(Main.getInstance().getNicknameWithUUID(), Main.getInstance().getUuidLinkedToNickname());
+            return false;
+        } else if(args.length != 1){
             player.sendMessage(ChatColor.translateAlternateColorCodes('&', Main.getInstance().getConfig().getString("messages.system-prefix")) + "§cErreur §8: §e/nick <nickname>");
             return false;
         }
 
+
         Main.getInstance().getAccount(player).ifPresent(account -> {
             account.getDataStatistique().setNickname(args[0]);
-            player.sendMessage("§aVotre nouveau pseudo est §7: " + PlayerUtils.getByString(account.getDataStatistique().getColorName()) + account.getDataStatistique().getNickname());
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', Main.getInstance().getConfig().getString("messages.system-prefix")) + "§aVotre nouveau pseudo est §7: " + PlayerUtils.getByString(account.getDataStatistique().getColorName()) + account.getDataStatistique().getNickname());
             player.setDisplayName(PlayerUtils.getByString(account.getDataStatistique().getColorName()) + account.getDataStatistique().getNickname());
         });
 
         YamlConfiguration config = YamlConfiguration.loadConfiguration(Main.getInstance().getNicknameWithUUID());
         config.set(player.getUniqueId().toString(), args[0].toLowerCase());
         FileUtils.save(config, Main.getInstance().getNicknameWithUUID());
+        if(!Main.getInstance().getUuidLinkedToNickname().isEmpty())
+            Main.getInstance().getUuidLinkedToNickname().clear();
+        FileUtils.fillMapWithAllLineOfFile(Main.getInstance().getNicknameWithUUID(), Main.getInstance().getUuidLinkedToNickname());
 
         return false;
     }
